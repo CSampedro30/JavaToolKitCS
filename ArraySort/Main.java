ab86a8ba255601126d665bfb1c4aac070563aedb
csampedro <c.sampedro@digital-box.fr>
2021-04-12 10:30:25 +0200

    added some stuff

---
+++ ArraySort/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import utils.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("This is tool for Array Sort");
+
+        int[] t = {1, 8, 3, 6};
+        Array.printArray(t);
+
+        int[] t2 = Array.genArray(20);
+        Array.printArray(t2);
+        
+    }
+}
---
+++ ArraySort/utils/Array.java
@@ -0,0 +1,56 @@
+package utils;
+
+public class Array {
+    public static void printArray(int[] t) {
+        int len = t.length;
+        System.out.print("[");
+        if (len >= 1) {
+            System.out.print(t[0]);
+        }
+        for (int i = 1; i < len; i++) {
+            System.out.print(", " + t[i]);
+        }
+        System.out.println("]");
+    }
+
+    public static void printArray(String[] t) {
+        int len = t.length;
+        System.out.print("[");
+        if (len >= 1) {
+            System.out.print(t[0]);
+        }
+        for (int i = 1; i < len; i++) {
+            System.out.print(", " + t[i]);
+        }
+        System.out.println("]");
+    }
+
+    public static int[] genArray(int size) {
+        int[] t = new int[size];
+        for (int i = 0; i < t.length; i++) {
+            t[i] = (int) (Math.random() * 101);
+        }
+        return t;
+    }
+
+    public static int findMax(int[] t, int n) {
+        int max = t[0];
+        int iMax = 0;
+        for (int i = 1; i < n; i++) {
+            if (max < t[i]) {
+                max = t[i];
+                iMax = i;
+            }
+        }
+        return iMax;
+    }
+
+    public static void sort(int[] t) {
+        for (int i = t.length; i > 0; i--) {
+            int iMax = findMax(t, i);
+            int tmp = t[i - 1];
+            t[i - 1] = t[iMax];
+            t[iMax] = tmp;
+        }
+    }
+}
